f2d907f3c0d35e0032c7c7367f1e9c784f7706aa
genus555 <yunxiaoche@gmail.com>
2025-08-01 22:09:24 -0700

    add HTMLNode

---
+++ src/htmlnode.py
@@ -0,0 +1,20 @@
+class HTMLNode():
+    def __init__(self, tag=None, value=None, children=None, props=None):
+        self.tag = tag
+        self.value = value
+        self.children = children
+        self.props = props
+
+    def to_html(self):
+        raise NotImplementedError
+    
+    def props_to_html(self):
+        if self.props == None:
+            raise Exception("No Props")
+        rtn_str = ""
+        for prop in self.props:
+            rtn_str = f"{rtn_str} {prop}=\"{self.props[prop]}\""
+        return rtn_str
+    
+    def __repr__(self):
+        return f"HTMLNode({self.tag}, {self.value}, {self.children}, {self.props})"
